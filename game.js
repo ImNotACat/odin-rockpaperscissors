14091a071ad364379c1b50e6a457f0304964a72d
Katya <k.vezzadini@gmail.com>
2022-02-04 18:06:33 +0000

    First game draft

--- game.js
+++ game.js
@@ -1,22 +1,85 @@
 function computerPlay() {
     // randomly enters rock, paper or scissors
-    var options = ["Rock", "Paper", "Scissors"];
+    var options = ["rock", "paper", "scissors"];
     const randomPick = options[Math.floor(Math.random() * options.length)];
-    console.log("Computer plays: " + randomPick);
+    return randomPick;
 }
 
 function playRound(playerSelection, computerSelection) {
 
-    // code - win or lose? Return a string to state the result
-    // make it case insensitive
-    // PROMPT user for selection
+    // PROMPT user for selection 
+    var userPick = window.prompt("Rock, paper or scissors?");
+    var computerSelection = computerPlay();
 
-//const playerSelection = "rock";
-//const computerSelection = computerPlay();
+    // make it case insensitive
+    userPick.toLowerCase;
+
+    if (userPick == "rock") {
+        if (computerSelection == "rock") {
+            return "tie";
+        }
+        else if (computerSelection == "paper") {
+            return "lost";
+        }
+        else {
+            return "won";
+        }
+    }
+    else if (userPick == "paper") {
+        if (computerSelection == "rock") {
+            return "won";
+        }
+        else if (computerSelection == "paper") {
+            return "tie";
+        }
+        else {
+            return "lost";
+        }
+    }
+    else if (userPick == "scissors") {
+        if (computerSelection == "rock") {
+            return "lost";
+        }
+        else if (computerSelection == "paper") {
+            return "won";
+        }
+        else {
+            return "tie";
+        }
+    }
+    else {
+        return "error";
+    }
 }
 
 function game() {
     // play playRound 5 times
-    // keep score
-    // report winner
+    var playerScore = 0;
+    var computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+
+        result = playRound();
+        if (result == "won") {
+            console.log ("You won!");
+            playerScore++;
+        }
+        else if (result == "lost") {
+            console.log ("You lost :(");
+            computerScore++;
+        }
+        else if (result == "tie") {
+            console.log ("It's a tie");
+        }
+        else {
+            console.log ("Sorry, there was an error. Try again!");
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log ("You have won the game! Hurrah! The score is " + playerScore + " to " + computerScore);
+    }
+    else if (playerScore < computerScore) {
+        console.log ("You have won lost the game, sad times. The score is " + playerScore + " to " + computerScore);
+    }
 }
