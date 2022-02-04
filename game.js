4236f9f2e1e33e4d9334907c3b86c410f0eaa336
Katya <k.vezzadini@gmail.com>
2022-02-04 16:02:38 +0000

    First draft

---
+++ game.js
@@ -0,0 +1,22 @@
+function computerPlay() {
+    // randomly enters rock, paper or scissors
+    var options = ["Rock", "Paper", "Scissors"];
+    const randomPick = options[Math.floor(Math.random() * options.length)];
+    console.log(randomPick);
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    // code - win or lose? Return a string to state the result
+    // make it case insensitive
+    // PROMPT user for selection
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+}
+
+function game() {
+    // play playRound 5 times
+    // keep score
+    // report winner
+}
