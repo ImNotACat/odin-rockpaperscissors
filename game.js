96e3575ba358b86114a27184f65db3171eb33c85
Katya <k.vezzadini@gmail.com>
2022-02-05 21:50:52 +0000

    Added better error handling

--- game.js
+++ game.js
@@ -5,10 +5,11 @@ function computerPlay() {
     return randomPick;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(number) {
 
     // PROMPT user for selection 
-    var userPick = window.prompt("Rock, paper or scissors?");
+    //var userPick = window.prompt("Rock, paper or scissors?");
+    var userPick = window.prompt("Round " + number + " of 5. Rock, paper or scissors?");
     var computerSelection = computerPlay();
 
     // make it case insensitive
@@ -16,34 +17,43 @@ function playRound(playerSelection, computerSelection) {
 
     if (userPick == "rock") {
         if (computerSelection == "rock") {
+            console.log("You played rock. Computer played rock.");
             return "tie";
         }
         else if (computerSelection == "paper") {
+            console.log("You played rock. Computer played paper.");
             return "lost";
         }
         else {
+            console.log("You played rock. Computer played scissors.");
             return "won";
         }
     }
     else if (userPick == "paper") {
         if (computerSelection == "rock") {
+            console.log("You played paper. Computer played rock.");
             return "won";
         }
         else if (computerSelection == "paper") {
+            console.log("You played paper. Computer played paper.");
             return "tie";
         }
         else {
+            console.log("You played paper. Computer played scissors.");
             return "lost";
         }
     }
     else if (userPick == "scissors") {
         if (computerSelection == "rock") {
+            console.log("You played scissors. Computer played rock.");
             return "lost";
         }
         else if (computerSelection == "paper") {
+            console.log("You played scissors. Computer played paper.");
             return "won";
         }
         else {
+            console.log("You played scissors. Computer played scissors.");
             return "tie";
         }
     }
@@ -59,7 +69,7 @@ function game() {
 
     for (let i = 0; i < 5; i++) {
 
-        result = playRound();
+        result = playRound(i+1);
         if (result == "won") {
             console.log ("You won!");
             playerScore++;
@@ -70,6 +80,9 @@ function game() {
         }
         else if (result == "tie") {
             console.log ("It's a tie");
+        }
+        else if (result == "error") {
+            i--
         }
         else {
             console.log ("Sorry, there was an error. Try again!");
